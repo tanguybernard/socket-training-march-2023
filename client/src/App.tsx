3d941dc62490fd1bd9a3613262d3bf3055c659fe
tanguybernard <bernardtanguy1pro@gmail.com>
2023-03-11 10:06:34 +0100

    fix navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,20 +8,20 @@ import Login from "./components/Login";
 import ChatPage from "./pages/ChatPage";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Header/Navbar";
+import NavbarLayout from "./components/NavbarLayout";
 const socket = io(process.env.REACT_APP_SOCKET_URL as string);
 
 function App() {
     return (
 
         <BrowserRouter>
-            <Navbar />
-                <div>
                     <Routes>
                         <Route path="/" element={<Login socket={socket} />}></Route>
-                        <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
-                        <Route path="/home" element={<HomePage />}></Route>
+                        <Route element={<NavbarLayout/>}>
+                            <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+                            <Route path="/home" element={<HomePage />}></Route>
+                        </Route>
                     </Routes>
-                </div>
             </BrowserRouter>
     );
 }
